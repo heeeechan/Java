593c7ebd6a0a4371f698cd7fbd2788e3bca9970c
heechan <heechan0023@naver.com>
2023-08-20 14:58:41 +0900

    Java Practice

---
+++ chap01/src/HonGongJava/KeyCodeExample.java
@@ -0,0 +1,12 @@
+package HonGongJava;
+
+public class KeyCodeExample {
+	public static void main(String[] args) throws Exception {
+		int keyCode;
+
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+		}
+	}
+}
