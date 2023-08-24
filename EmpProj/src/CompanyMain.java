338b8b4115b2e170406c9e33c088c7dedd670338
heechan <heechan0023@naver.com>
2023-08-24 10:05:36 +0900

    230824 Java Practice

---
+++ EmpProj/src/CompanyMain.java
@@ -0,0 +1,25 @@
+import emp.PartTime;
+import emp.Permanent;
+import emp.Sales;
+
+public class CompanyMain {
+	public static void main(String[] args) {
+		Company com = new Company();
+		Permanent emp1 = new Permanent(1001, "김희찬", 5000000);
+		Sales emp2 = new Sales(1002, "황희찬", 4000000, 1000000);
+		PartTime emp3 = new PartTime(1003, "서희찬", 160, 30000);
+
+		com.enter(emp1);
+		com.enter(emp2);
+		com.enter(emp3);
+
+		com.allEmployeeInfo();
+		System.out.println("총급여액:" + com.getTotalPay());
+	}
+}
+/*
+사번:1001,이름:김희찬,급여:5000000
+사번:1002,이름:황희찬,급여:5000000
+사번:1003,이름:서희찬,급여:4800000
+총급여액:14800000
+*/
