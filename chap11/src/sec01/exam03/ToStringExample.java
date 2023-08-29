95caf412027f50e89834bda9218865481c21e544
heechan <heechan0023@naver.com>
2023-08-29 11:10:46 +0900

    230829 Java Practice

--- chap11/src/sec01/exam03/ToStringExample.java
+++ chap11/src/sec01/exam03/ToStringExample.java
@@ -1,5 +1,12 @@
 package sec01.exam03;
 
-public class ToStringExample {
+import java.util.Date;
 
+public class ToStringExample {
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+	}
 }
