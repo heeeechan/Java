8b42a7b3140ea8b834d6aa500304a17e59acb358
heechan <heechan0023@naver.com>
2023-09-25 22:36:32 +0900

    230925 Java Practice

--- JavaJungsuk/src/chap06/Ex6_9.java
+++ JavaJungsuk/src/chap06/Ex6_9.java
@@ -3,6 +3,22 @@ package chap06;
 class MyMath2 {
 	long a, b;
 
+	// 인스턴스 변수 a, b만을 이용해서 작업하므로 매개변수가 필요없다.
+	long add() {
+		return a + b;
+	}
+
+	long subtract() {
+		return a - b;
+	}
+
+	long multiply() {
+		return a * b;
+	}
+
+	double divide() {
+		return a / b;
+	}
 }
 
 public class Ex6_9 {
